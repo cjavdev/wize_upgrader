b5094f22476c1603b6492f2810b94d779a14b3d6
CJ Avilla <cjavilla@gmail.com>
2014-02-21 17:03:34 -0800

    adding jbuilder to the list of gems that shouldn't be copied

--- lib/wize_upgrader.rb
+++ lib/wize_upgrader.rb
@@ -20,6 +20,7 @@ module Wize
     COMMON_GEMS = [
       "gem 'jquery-rails'",
       "gem 'rspec-rails'",
+      "gem 'jbuilder'",
       "gem 'coffee-rails', '~> 3.2.1'",
       "gem 'sass-rails',   '~> 3.2.3'",
       "gem 'uglifier', '>= 1.0.3'",
